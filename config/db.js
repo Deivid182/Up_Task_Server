494ce14f882e5af3615f26523ec2f6d8a3ae83a9
Dave <davidmendoza911119@gmail.com>
2023-08-29 21:20:45 -0600

    fixed an uri error

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 export const connectDB = async() => {
+
+  dotenv.config();
   try {
     await mongoose.connect(
       process.env.MONGODB_URL, {
